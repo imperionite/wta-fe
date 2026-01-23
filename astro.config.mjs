d9a96d9c1db678c9cdd41c66ac2ccc756db73255
imperionite <lr.aimperial@mmdc.mcl.edu.ph>
2026-01-23 18:39:20 +0800

    edit files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://arnelimperial.github.io/access/",
-  base: "/access/", 
-});
+export default defineConfig({});
